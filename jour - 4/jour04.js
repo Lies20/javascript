d9116c71162accf9cf35ace4357fcc6be8f99faf
lies93 <hocini.lies@gmail.com>
2022-01-30 11:46:06 +0100

    Exercice 2 50%

--- jour - 4/jour04.js
+++ jour - 4/jour04.js
@@ -11,3 +11,30 @@
 //     console.log("true");
 // }
 
+
+//   Exercice 2 combine 
+
+let cat = {
+        name:"garfield",
+        ages : 5,
+        isCute : true,
+    };
+    
+let cat2 = {
+    name : "bijou",
+    age : 23,
+    isCute : false,
+};
+
+//   cat = cat.length
+
+let cats = [cat, cat2]
+//  console.log(cats);
+//  console.log( cats.values[1]);
+
+ for (i=2;i<cats.length;i++)
+{console.log(cats[i]); 
+}
+ 
+
+    
