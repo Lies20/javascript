90531ca07408e5cdf7fe6b5a3bd18135359490fc
lies93 <hocini.lies@gmail.com>
2022-01-31 17:08:51 +0100

    Stand by exercice 04

---
+++ boucles/ForLoops.js
@@ -0,0 +1,39 @@
+
+// Exercice 1 Somme des carrés
+
+//    total = 0;
+
+// for(i = 5 ; i <= 10 ; i++ ){
+//     // console.log(i)
+//     total = i*i + total;
+// }
+// console.log(total);
+
+// Exercice 2 comptons 
+// let result="";
+
+//  for(i = 100 ; i < 1000;i++){
+//     if(i % 7 === 0){
+//         result ++
+//         // console.log(result);
+//     }
+// }
+//     console.log(result);
+
+
+// Exercice 3 chanceux 
+
+// let total = 0;
+
+// for (i = 0; i < 20; i++) {
+//   let   result = (Math.ceil(Math.random() * 6));
+//     console.log(result);
+//         if (result >= 5) {
+//             total = result + total
+//         }
+    // }
+
+// console.log(total);
+
+ 
+ // Exercice 04 Des boites 
