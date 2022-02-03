38c934cd50388d4135cdedc89caf2a1ee3ef53a5
lies93 <hocini.lies@gmail.com>
2022-02-03 14:12:20 +0100

    Day7 Ex3

---
+++ jour7/jour7.js
@@ -0,0 +1,20 @@
+    // Exercice 1 file system
+
+// var fs = require("fs");
+// fs.readFile("jour07.txt", function (err,data) {
+//     if (err) {
+//         console.error(err);
+//         return;
+        
+//     }
+//     console.log("lecture en  différé :" + data.toString())
+// });
+
+
+ //  Exercice 2 Map Double 
+
+ let array = [1,2,3,4,5];
+ var doubles = array.map(function(array) {
+    return array * 2;
+  });
+  console.log(doubles);
