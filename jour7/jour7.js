eb9b815282306e035d2ec21649008de38716a1b3
lies93 <hocini.lies@gmail.com>
2022-02-03 16:06:12 +0100

    Exercice 5 Finish

--- jour7/jour7.js
+++ jour7/jour7.js
@@ -60,4 +60,8 @@
 
         // Exercice 5 filter even
 
-        
+        let numbers = [1,2,3,4,5,6,7,8]
+        let even = numbers.filter(function(value){
+             return value % 2 === 0 
+            })
+             console.log(even)
