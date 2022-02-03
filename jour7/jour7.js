c5e09f8d68b75080fe9293e5c928c5b0060f042a
lies93 <hocini.lies@gmail.com>
2022-02-03 16:22:57 +0100

    Exercice 6 finish

--- jour7/jour7.js
+++ jour7/jour7.js
@@ -1,17 +1,17 @@
-    // Exercice 1 file system
+// Exercice 1 file system
 
 // var fs = require("fs");
 // fs.readFile("jour07.txt", function (err,data) {
 //     if (err) {
 //         console.error(err);
 //         return;
-        
+
 //     }
 //     console.log("lecture en  différé :" + data.toString())
 // });
 
 
- //  Exercice 2 Map Double 
+//  Exercice 2 Map Double 
 
 //  let array = [1,2,3,4,5];
 //  var doubles = array.map(function(array) {
@@ -20,48 +20,91 @@
 //   console.log(doubles);
 
 
-    // Exercice 3 Map Names
+// Exercice 3 Map Names
 
-    // let longNames=[
-    //     {
-    //         firstName: "Jane",
-    //         lastName: "Doe"
-    //     },
-    //     {
-    //         firstName: "John",
-    //         lastName: "Smith"
-    //     }
-    // ]
+// let longNames=[
+//     {
+//         firstName: "Jane",
+//         lastName: "Doe"
+//     },
+//     {
+//         firstName: "John",
+//         lastName: "Smith"
+//     }
+// ]
 
-    // let shortNames = longNames.map(  function (person){
-    //     // console.log("affichage de person", person);
-    //     let finallyName = {
-    //         name : person.firstName + " " + person.lastName      
-    //     }
-    //     //  let array = [finallyName];
+// let shortNames = longNames.map(  function (person){
+//     // console.log("affichage de person", person);
+//     let finallyName = {
+//         name : person.firstName + " " + person.lastName      
+//     }
+//     //  let array = [finallyName];
 
-    //     // console.log(finallyName);
-    //     return finallyName;
-    
-    // })
+//     // console.log(finallyName);
+//     return finallyName;
 
-    // console.log(shortNames);
+// })
 
-     // Exercice 4 filter numbers 
+// console.log(shortNames);
 
-    //   let array = [ 1 , "toto",34,"javascript",8]
-      
-    //   let numbers = array.filter(function(num){
-    //       if(typeof num === "number" )
-    //       return num
-    //   })
-    //   console.log(numbers);
-     
+// Exercice 4 filter numbers 
 
-        // Exercice 5 filter even
+//   let array = [ 1 , "toto",34,"javascript",8]
 
-        let numbers = [1,2,3,4,5,6,7,8]
-        let even = numbers.filter(function(value){
-             return value % 2 === 0 
-            })
-             console.log(even)
+//   let numbers = array.filter(function(num){
+//       if(typeof num === "number" )
+//       return num
+//   })
+//   console.log(numbers);
+
+
+// Exercice 5 filter even
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+// let even = numbers.filter(function (value) {
+//     return value % 2 === 0
+// })
+// console.log(even)
+
+
+// Exercice 6 Cake 
+
+let cakes = [
+    {
+        name: "cake",
+        flavor: "vanilla",
+        status: "available"
+    },
+    {
+        name: "brownie",
+        flavor: "chocolate",
+        status: "available"
+    },
+    {
+        name: "pie",
+        flavor: "strawberry",
+        status: "available"
+    },
+    {
+        name: "muffin",
+        flavor: "pistachio",
+        status: "available"
+    },
+    {
+        name: "donut",
+        flavor: "chocolate",
+        status: "available"
+    },
+]
+
+var soldOut = cakes.filter(function (cake) {
+    return cake.flavor === "chocolate"
+})
+soldOut = soldOut.map(function(cake){
+       cake.status = 'is sold out '
+    return cake
+
+})
+
+
+console.log(soldOut);
