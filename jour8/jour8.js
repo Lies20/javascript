0de2963188d44fb3e08ede553af8effe1fb6187f
lies93 <hocini.lies@gmail.com>
2022-02-07 15:27:32 +0100

    Exercice 1  of json done

---
+++ jour8/jour8.js
@@ -0,0 +1,27 @@
+
+// const request = require("request");
+ 
+// let getCountries = [];
+
+// request.get("https://restcountries.com/v3.1/all", function (err, res, body) {
+//     // console.log(err);
+//     // console.log(res.statusCode);
+//     // console.log(body);
+    
+//     let country = JSON.parse(body);
+// //     console.log(country);
+
+// //    let getCountries = country[2].name.common
+// //    console.log(getCountries);
+
+//    for(i=0  ; i < country.length ; i++){
+//     //    console.log(country[i].name.common);
+//         getCountries.push(country[i].name.common)
+        
+//    }
+//    console.log(getCountries);
+
+// });
+
+
+
