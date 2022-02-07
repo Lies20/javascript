0c42cd2431ea4a82883f82b5d965038422c13fba
lies93 <hocini.lies@gmail.com>
2022-02-07 16:30:09 +0100

    Exercice 2 of json Done

--- jour8/jour8.js
+++ jour8/jour8.js
@@ -1,13 +1,13 @@
 
 // const request = require("request");
- 
+
 // let getCountries = [];
 
 // request.get("https://restcountries.com/v3.1/all", function (err, res, body) {
 //     // console.log(err);
 //     // console.log(res.statusCode);
 //     // console.log(body);
-    
+
 //     let country = JSON.parse(body);
 // //     console.log(country);
 
@@ -17,11 +17,24 @@
 //    for(i=0  ; i < country.length ; i++){
 //     //    console.log(country[i].name.common);
 //         getCountries.push(country[i].name.common)
-        
+
 //    }
 //    console.log(getCountries);
 
 // });
 
+// Exercice 2 Chuck Norris 
 
+var request = require("request");
 
+request.get("https://api.chucknorris.io/jokes/random", function  (err, res, body) {
+    // console.log(err);
+    // console.log(res.statusCode);
+    console.log(body);
+
+    let getJoke = JSON.parse(body);
+    console.log(getJoke);
+
+    let joke = getJoke.value;
+    console.log(joke);
+});
